40435476ba11ba24c4dae0b9e18ad18e2fb9d36f
Jorge <jbsbookme@gmail.com>
2026-04-03 22:16:54 -0400

    Strip legacy routes

--- app/app/book/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function BookPage() {
-  return <div>Booking page</div>;
-}
--- app/twitter-image.tsx
+++
@@ -1,4 +0,0 @@
-export { default } from './opengraph-image';
-
-export const runtime = 'edge';
-export { size, contentType } from './opengraph-image';
